54c61d4b758c464b363abc7784e6c7380724d995
Carter Sande <csande@bloomberg.net>
2019-01-10 09:45:48 -0800

    Add clang-format autoformatting from RECC

--- buildbox-common.h
+++ buildbox-common.h
@@ -26,36 +26,37 @@ using namespace build::bazel::remote::execution::v2;
 #define BUFFER_SIZE (1024 * 1024)
 
 class Client {
-public:
-	void init(const char *remote_url, const char *server_cert, const char *client_key, const char *client_cert);
-	void download(int fd, const Digest& digest);
-	void upload(int fd, const Digest& digest);
-	bool batch_upload_add(const Digest& digest, const std::vector<char>& data);
-	bool batch_upload();
-	bool batch_download_add(const Digest& digest);
-	bool batch_download_next(const Digest **digest, const std::string **data);
+  public:
+    void init(const char *remote_url, const char *server_cert,
+              const char *client_key, const char *client_cert);
+    void download(int fd, const Digest &digest);
+    void upload(int fd, const Digest &digest);
+    bool batch_upload_add(const Digest &digest, const std::vector<char> &data);
+    bool batch_upload();
+    bool batch_download_add(const Digest &digest);
+    bool batch_download_next(const Digest **digest, const std::string **data);
 
-	int64_t max_batch_total_size_bytes;
+    int64_t max_batch_total_size_bytes;
 
-private:
-	std::shared_ptr<grpc::Channel> channel;
-	std::unique_ptr<ByteStream::Stub> bytestream_client;
-	std::unique_ptr<ContentAddressableStorage::Stub> cas_client;
-	std::unique_ptr<Capabilities::Stub> capabilities_client;
+  private:
+    std::shared_ptr<grpc::Channel> channel;
+    std::unique_ptr<ByteStream::Stub> bytestream_client;
+    std::unique_ptr<ContentAddressableStorage::Stub> cas_client;
+    std::unique_ptr<Capabilities::Stub> capabilities_client;
 
-	std::string uuid;
+    std::string uuid;
 
-	BatchUpdateBlobsRequest batch_update_request;
-	BatchUpdateBlobsResponse batch_update_response;
-	int64_t batch_update_size;
+    BatchUpdateBlobsRequest batch_update_request;
+    BatchUpdateBlobsResponse batch_update_response;
+    int64_t batch_update_size;
 
-	std::unique_ptr<grpc::ClientContext> batch_read_context;
-	BatchReadBlobsRequest batch_read_request;
-	BatchReadBlobsResponse batch_read_response;
-	BatchReadBlobsResponse_Response batch_read_blob_response;
-	int64_t batch_read_size;
-	bool batch_read_request_sent;
-	int batch_read_response_index;
+    std::unique_ptr<grpc::ClientContext> batch_read_context;
+    BatchReadBlobsRequest batch_read_request;
+    BatchReadBlobsResponse batch_read_response;
+    BatchReadBlobsResponse_Response batch_read_blob_response;
+    int64_t batch_read_size;
+    bool batch_read_request_sent;
+    int batch_read_response_index;
 };
 
 #endif
