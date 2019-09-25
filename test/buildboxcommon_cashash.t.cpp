a52c22aca71a27c5e9f12d193145ca72263f1bf3
Santiago Gil <santiago.gil@codethink.co.uk>
2019-09-25 10:21:56 +0100

    `buildboxcommon_cashash.t.cpp`: add copyright year

--- test/buildboxcommon_cashash.t.cpp
+++ test/buildboxcommon_cashash.t.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Bloomberg Finance LP
+ * Copyright 2018-2019 Bloomberg Finance LP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
