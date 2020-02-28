ae9f11212568a48a7bdc14c6e1fb4d6582b46458
Richard Kennedy <rkennedy76@bloomberg.net>
2020-02-28 22:44:29 +0000

    add metric testing util

---
+++ buildbox-common/buildboxcommonmetrics/buildboxcommonmetrics_testingutils.h
@@ -0,0 +1,50 @@
+// Copyright 2020 Bloomberg Finance L.P
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_BUILDBOXCOMMONMETRICS_TESTINGUTILS_H
+#define INCLUDED_BUILDBOXCOMMONMETRICS_TESTINGUTILS_H
+
+#include <buildboxcommonmetrics_metriccollectorfactory.h>
+
+namespace buildboxcommon {
+namespace buildboxcommonmetrics {
+
+template <typename MetricType>
+bool validateMetricCollection(const std::string &metric)
+{
+    typedef decltype(std::declval<MetricType>().value()) ValueType;
+    MetricCollector<ValueType> *collector =
+        MetricCollectorFactory::getCollector<ValueType>();
+    auto metrics_map = collector->getSnapshot();
+    return metrics_map.count(metric);
+}
+
+template <typename MetricType>
+bool validateMetricCollection(const std::vector<std::string> &metrics)
+{
+    typedef decltype(std::declval<MetricType>().value()) ValueType;
+    MetricCollector<ValueType> *collector =
+        MetricCollectorFactory::getCollector<ValueType>();
+    auto metrics_map = collector->getSnapshot();
+    for (auto &metric : metrics) {
+        if (metrics_map.find(metric) == metrics_map.end()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+} // namespace buildboxcommonmetrics
+} // namespace buildboxcommon
+#endif
