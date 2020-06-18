897b341620549fcafc25dbe464eaab682d7ebf89
Santiago Gil <santiago.gil@codethink.co.uk>
2020-06-18 18:20:39 +0100

    Add `StandardOutputStreamer` class
    This class provides a high-level interface of streaming standard output
    files, connecting a `StreamingStandardOutputFileMonitor` with a
    `LogStreamWriter` instance.

---
+++ buildbox-common/buildboxcommon/buildboxcommon_standardoutputstreamer.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 Bloomberg Finance LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <buildboxcommon_standardoutputstreamer.h>
+
+#include <buildboxcommon_logging.h>
+
+#include <functional>
+
+namespace buildboxcommon {
+
+StandardOutputStreamer::StandardOutputStreamer(
+    const std::string &path, const ConnectionOptions &connectionOptions,
+    const std::string &resourceName)
+    : d_filePath(path), d_url(connectionOptions.d_url),
+      d_resourceName(resourceName),
+      d_logstreamWriter(d_resourceName, connectionOptions),
+      d_fileMonitor(d_filePath,
+                    std::bind(&StandardOutputStreamer::streamLogChunk, this,
+                              std::placeholders::_1))
+{
+}
+
+StandardOutputStreamer::~StandardOutputStreamer()
+{
+    if (!d_stopRequested) {
+        BUILDBOX_LOG_WARNING("Destroying `StandardOutputStreamer` instance "
+                             "without invoking `stop()`, outputs may be lost");
+    }
+}
+
+void StandardOutputStreamer::stop()
+{
+    if (!d_stopRequested) {
+        BUILDBOX_LOG_DEBUG("Stopping the monitoring of ["
+                           << d_filePath << "] and committing the log");
+        d_fileMonitor.stop();
+        d_logstreamWriter.commit();
+    }
+
+    d_stopRequested = true;
+}
+
+void StandardOutputStreamer::streamLogChunk(
+    const StreamingStandardOutputFileMonitor::FileChunk &chunk)
+{
+    BUILDBOX_LOG_DEBUG("File monitor reported ["
+                       << d_filePath << "] has " << chunk.size()
+                       << " bytes available, streaming to "
+                       << "[" << d_url << "/" << d_resourceName << "]");
+
+    d_logstreamWriter.write(std::string(chunk.ptr(), chunk.size()));
+}
+
+} // namespace buildboxcommon
---
+++ buildbox-common/buildboxcommon/buildboxcommon_standardoutputstreamer.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Bloomberg Finance LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INCLUDED_BUILDBOXCOMMON_STANDARDOUTPUTSTREAMER
+#define INCLUDED_BUILDBOXCOMMON_STANDARDOUTPUTSTREAMER
+
+#include <buildboxcommon_connectionoptions.h>
+#include <buildboxcommon_logstreamwriter.h>
+#include <buildboxcommon_streamingstandardoutputfilemonitor.h>
+
+#include <string>
+
+namespace buildboxcommon {
+
+class StandardOutputStreamer {
+  public:
+    /* Given a path to a file, a ByteStream endpoint and a resource name,
+     * stream the contents of that file as they are updated.
+     *
+     * The file must be written in an append-only manner.
+     */
+    explicit StandardOutputStreamer(const std::string &path,
+                                    const ConnectionOptions &connectionOptions,
+                                    const std::string &resourceName);
+
+    ~StandardOutputStreamer();
+
+    // Stop monitoring the file, issue a `finish_write` request and close the
+    // connection.
+    // If the request fails, it throws a `GrpcError` exception.
+    void stop();
+
+  private:
+    void
+    streamLogChunk(const StreamingStandardOutputFileMonitor::FileChunk &chunk);
+
+    const std::string d_filePath;
+    const std::string d_url;
+    const std::string d_resourceName;
+
+    LogStreamWriter d_logstreamWriter;
+    StreamingStandardOutputFileMonitor d_fileMonitor;
+
+    bool d_stopRequested;
+};
+} // namespace buildboxcommon
+
+#endif
