sdclarke/buildbox-common

commit f60f595719d42d7680696c55e6b3bf085782f0b0

Do not install libglog in Dockerfile
The debian:buster version of libglog currently is 0.3.5 which has issues with it's .pc(pkg-config) file.
Don't explicitly install it in the Docker file which will cause CMake to download version 0.4.0 from github which has a fixed .pc file

cmake/BuildboxGlogSetup.cmake
@@ -28,33 +28,18 @@ find_package(glog CONFIG)
 if (glog_FOUND)
   message(STATUS "Found glog CMake Config")
   set(GLOG_TARGET glog::glog)
-else()
-    find_package(PkgConfig)
-    pkg_check_modules(glog IMPORTED_TARGET libglog)
-    if (glog_FOUND)
-      message(STATUS "Found glog via pkg-config")
-      set(GLOG_TARGET PkgConfig::glog)
-    endif()
-    # This is a workaround that includes libgflags for statically
-    # linked applications. The shared library libglog.so contains
-    # meta-data on it's dependencies which includes libgflags
-    # in particular. As a result, link failures are not a problem when
-    # linking dynamically, but it is when linking statically. Seeing
-    # libgflags is included on the link-line indirectly for dynamically
-    # linked applications, it shouldn't be harmful to explicitly include
-    # here.
-    # $ readelf -d /usr/lib/x86_64-linux-gnu/libglog.so
-    #   Tag        Type                         Name/Value
-    #   0x0000000000000001 (NEEDED)             Shared library: [libgflags.so.2.2]
-    #   0x0000000000000001 (NEEDED)             Shared library: [libunwind.so.8]
-    #   0x0000000000000001 (NEEDED)             Shared library: [libpthread.so.0]
-    #   0x0000000000000001 (NEEDED)             Shared library: [libstdc++.so.6]
-    #   0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
-    pkg_check_modules(gflags IMPORTED_TARGET gflags)
-    if (gflags_FOUND)
-      message(STATUS "Found gflags via pkg-config")
-      set(GFLAGS_TARGET PkgConfig::gflags)
-    endif()
+elseif()
+  find_package(PkgConfig)
+  pkg_check_modules(glog IMPORTED_TARGET libglog)
+  if (glog_FOUND)
+    message(STATUS "Found glog via pkg-config")
+    set(GLOG_TARGET PkgConfig::glog)
+  endif()
+  pkg_check_modules(gflags IMPORTED_TARGET gflags)
+  if (gflags_FOUND)
+    message(STATUS "Found gflags via pkg-config")
+    set(GFLAGS_TARGET PkgConfig::gflags)
+  endif()
 endif()
 
 if (glog_FOUND)
@@ -67,4 +52,5 @@ else()
   message(STATUS "Attempting to fetch glog from github")
   load_glog()
   set(GLOG_TARGET glog)
+  message(STATUS "Found glog via github: ${GLOG_TARGET}")
 endif()

Dockerfile
@@ -14,7 +14,6 @@ RUN apt-get update && apt-get install -y \
     googletest \
     grpc++ \
     libbenchmark-dev \
-    libgoogle-glog-dev \
     libssl-dev \
     pkg-config \
     uuid-dev \

commit 76692bd3cf061aef7506981dad233fc424194a06

cmake: Fix finding glog via pkg-config
Fixes: f60f5957 ("Do not install libglog in Dockerfile")

cmake/BuildboxGlogSetup.cmake
@@ -28,7 +28,7 @@ find_package(glog CONFIG)
 if (glog_FOUND)
   message(STATUS "Found glog CMake Config")
   set(GLOG_TARGET glog::glog)
-elseif()
+else()
   find_package(PkgConfig)
   pkg_check_modules(glog IMPORTED_TARGET libglog)
   if (glog_FOUND)